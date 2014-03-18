600a18391860c71ea37e01403600c27ba5469610
James Chevalier <jchevalier@gmail.com>
2014-03-18 12:15:12 -0400

    add spec for version

--- lib/content_block/version.rb
+++ lib/content_block/version.rb
@@ -1,3 +1,3 @@
 module ContentBlock
-  VERSION = "0.0.1"
+  VERSION = '0.0.1'
 end
---
+++ spec/content_block/version_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe 'ContentBlock::VERSION' do
+  it 'should be the correct version' do
+    ContentBlock::VERSION.should == '0.0.1'
+  end
+end
