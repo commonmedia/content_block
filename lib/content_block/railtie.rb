e336485e0ac0e61d90ad79f0d82fa5d2f5ed3e70
James Chevalier <jchevalier@gmail.com>
2014-04-17 13:33:20 -0400

    make content_block available in all controllers

--- lib/content_block/railtie.rb
+++ lib/content_block/railtie.rb
@@ -3,8 +3,13 @@ require 'content_block/helper'
 
 module ContentBlock
   class Railtie < Rails::Railtie
-    initializer 'content_block.action_view' do
+    initializer 'content_block' do
       ActiveSupport.on_load(:action_view) do
+        include ContentBlock::Helper
+      end
+
+      # Make ContentBlock available in all controllers
+      ActiveSupport.on_load(:action_controller) do
         include ContentBlock::Helper
       end
     end
