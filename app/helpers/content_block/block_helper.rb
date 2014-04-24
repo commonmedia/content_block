4175a175c985338af883bf20b7db0226ac94779b
James Chevalier <jchevalier@gmail.com>
2014-04-24 14:34:13 -0400

    handle unfound block in helper

--- app/helpers/content_block/block_helper.rb
+++ app/helpers/content_block/block_helper.rb
@@ -2,11 +2,12 @@ module ContentBlock
   module BlockHelper
     def rich_block(find_by)
       block = ContentBlock::RichBlock.where('id = ? OR name = ?', find_by, find_by).first
-      block.content.html_safe
+      block.try(:content).try(:html_safe)
     end
 
     def rich_block_with_title(find_by, wrapper)
       block         = ContentBlock::RichBlock.where('id = ? OR name = ?', find_by, find_by).first
+      return nil unless block.present?
       wrapped_title = "<#{wrapper}>#{block.title}</#{wrapper}>"
       "#{wrapped_title}#{block.content}".html_safe
     end
