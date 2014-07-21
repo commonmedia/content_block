66219ea9a8e0705a35544a670c20cd75a8d9ca72
James Chevalier <jchevalier@gmail.com>
2014-07-21 13:14:43 -0400

    remove ckeditor override, as the latest version doesn't require it

--- app/assets/javascripts/content_block/application.js
+++ app/assets/javascripts/content_block/application.js
@@ -10,6 +10,5 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require ckeditor/override
 //= require ckeditor/init
 //= require_tree .
